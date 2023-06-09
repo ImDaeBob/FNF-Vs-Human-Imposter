6d154494d82b4d37907fd9bb1cfc42e945ba739b
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-06-09 19:09:36 +0300

    update source/options/OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -37,14 +37,29 @@ class OptionsState extends MusicBeatState
 	function openSelectedSubstate(label:String) {
 		switch(label) {
 			case 'Note Colors':
+			  #if mobile
+        removeVirtualPad();
+        #end
 				openSubState(new options.NotesSubState());
 			case 'Controls':
+				#if mobile
+        removeVirtualPad();
+        #end
 				openSubState(new options.ControlsSubState());
 			case 'Graphics':
+				#if mobile
+        removeVirtualPad();
+        #end
 				openSubState(new options.GraphicsSettingsSubState());
 			case 'Visuals and UI':
+				#if mobile
+        removeVirtualPad();
+        #end
 				openSubState(new options.VisualsUISubState());
 			case 'Gameplay':
+				#if mobile
+        removeVirtualPad();
+        #end
 				openSubState(new options.GameplaySettingsSubState());
 			case 'Adjust Delay and Combo':
 				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
@@ -86,6 +101,22 @@ class OptionsState extends MusicBeatState
 		changeSelection();
 		ClientPrefs.saveSettings();
 
+   #if mobile
+   addVirtualPad(UP_DOWN, A_B_X_Y);
+
+		var sus:FlxText = new FlxText(10, 14, 0, 'Press X to customize your android controls', 16);
+		sus.setFormat(Paths.font('vcr.ttf'), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		sus.borderSize = 2.4;
+		sus.scrollFactor.set();
+		add(sus); //sus??!?!
+		
+		var sussy:FlxText = new FlxText(10, 32, 0, 'Press Y to customize opacity for your android controls', 16);
+		sussy.setFormat(Paths.font('vcr.ttf'), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		sussy.borderSize = 2.4;
+		sussy.scrollFactor.set();
+		add(sussy); //even more sus!
+   #end
+
 		super.create();
 	}
 
@@ -96,6 +127,18 @@ class OptionsState extends MusicBeatState
 
 	override function update(elapsed:Float) {
 		super.update(elapsed);
+
+    #if mobile
+		if (virtualPad.buttonX.justPressed)
+		{
+			removeVirtualPad();
+			openSubState(new mobile.MobileControlsSubState());
+		}
+		if (virtualPad.buttonY.justPressed) {
+			removeVirtualPad();
+			openSubState(new mobile.AndroidSettingsSubState());
+		}
+		#end
 
 		if (controls.UI_UP_P) {
 			changeSelection(-1);
